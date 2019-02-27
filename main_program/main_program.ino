ef7fc9ffc49ccf337c9c03e0af12e26d1dfe74bb
joliesla <34600363+joliesla@users.noreply.github.com>
2019-02-27 10:13:52 +0000

    Try to get control loop to work

--- main_program/main_program.ino
+++ main_program/main_program.ino
@@ -1,9 +1,12 @@
 #include "irSensor.h"
 #include "motor_control.h"
 #include "timer.h"
+#include "ultrasound.h"
 
 IrReadingColour irReadingColour;
+UsReadingControl usControl;
 RobotMotor motor;
+Timer debugTimer;
 Timer usTimer;
 long count = 0;
 int irSensorReading = 0;
@@ -12,13 +15,23 @@ bool startMotors = true;
 int readingDelay = 100;
 int readingDelayCounter = 0;
 
+//Constant
+const int US_SAMPLING_PERIOD = 10;
+
+//Debugging
+int a;
+
 void setup() {
   Serial.begin(9600);
   motor.start();
   motor.setMotors(1,4);
-  motor.setMotorSpeed(200, 200);
+  motor.setMotorSpeed(50, 50);
   irReadingColour.setPinNumber(A0);
+  usControl.setPins(2, 3);
+  usControl.setTargetDistance(20);
+  debugTimer.reset();
   usTimer.reset();
+  motor.runMotor(true, 'b');
 }
 
 void loop()
@@ -42,15 +55,22 @@ void loop()
     motor.runMotor(false, 'b');
   }*/
   /*
-  count++;
-  if (count == 1){
-    motor.runMotor(true, 'b');
+  if (debugTimer.getSeconds() == 3){
+    motor.nudge(1);
+  }*/
+
+  if (usTimer.getTime() > US_SAMPLING_PERIOD){
+    usControl.updateValue();
+    a = usControl.getValue();
+    //Serial.println(a);
+    Serial.println(motor.currentL);
+    Serial.println(motor.currentR);
+    Serial.println(usControl.getSpeedDiff());
+    usTimer.reset();
+    motor.adjustSpeed(usControl.getSpeedDiff());
   }
-  if (count == 5){
-    motor.adjustCourse(40);
+  if (debugTimer.getSeconds() >= 15){
+    motor.runMotor(false, 'b');
   }
-  delay(500);*/
-  delay(1000);
-  Serial.println(usTimer.getTime());
-  usTimer.reset();
+  
 }
